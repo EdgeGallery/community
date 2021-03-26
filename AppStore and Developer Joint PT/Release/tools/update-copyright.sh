65d9da5c5408422cd02051586a9cc17b18d87ba2
luoxiaoyun <hw.luoxiaoyun@huawei.com>
2021-03-26 17:52:52 +0800

    自动更新代码license

--- AppStore and Developer Joint PT/Release/tools/update-copyright.sh
+++ AppStore and Developer Joint PT/Release/tools/update-copyright.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-git clone https://gitee.com/edgegallery/user-mgmt.git --branch master
+git clone https://gitee.com/edgegallery/appstore-be.git --branch master
 
-cd user-mgmt
+cd appstore-be
 
 for filename in `git ls-tree -r --name-only HEAD`
 do
     addYear=$(git log --pretty=format:"%ad %H %an" -- $filename | tail -1 | awk '{print $5}')
     modYear=$(git log -1 --pretty=format:"%ad %H %an" -- $filename | awk '{print $5}')
     if [ $modYear -gt $addYear ]; then
-        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear-$modYear/g" $filename
+        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear-$modYear/" $filename
     elif [ $modYear -eq $addYear ]; then
-        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear/g" $filename
+        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear/" $filename
     fi
 done
